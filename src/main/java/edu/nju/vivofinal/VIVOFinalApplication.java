aa02f21e24dfb7a1f490d2f8a6c61f056fe6f227
JSTZCGM <980231201@qq.com>
2019-05-24 15:01:29 +0800

    init project.

---
+++ src/main/java/edu/nju/vivofinal/VIVOFinalApplication.java
@@ -0,0 +1,13 @@
+package edu.nju.vivofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VIVOFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VIVOFinalApplication.class, args);
+    }
+
+}
